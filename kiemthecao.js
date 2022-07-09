ca2f119344d005e174d27d2122bb4963fa9ad76a
KiemthecaoFree <62643397+kiemthecaofree@users.noreply.github.com>
2022-07-10 03:31:35 +0700

    Update kiemthecao.js

--- kiemthecao.js
+++ kiemthecao.js
@@ -1,20 +1,18 @@
-<script type="text/javascript">
-        var time = 61;
-        const myArray = [26265, 37277, 72615, 27275, 27715, 71717, 26116, 72717, 88732, 83861, 48827, 48711, 22627, 83883, 47772, 383838, 83822]
-        let interval;
-        var get_code = document.querySelector(".get_code ");
-        get_code.addEventListener("click", ()=>{
-            const changeTime = () =>{
-            time -- ;
-                if(time === 59){
+var time = 61;
+const myArray = [26265, 37277, 72615, 27275, 27715, 71717, 26116, 72717, 88732, 83861, 48827, 48711, 22627, 83883, 47772, 383838, 83822]
+let interval;
+var get_code = document.querySelector(".get_code ");
+get_code.addEventListener("click", ()=>{
+    const changeTime = () =>{
+    time -- ;
+        if(time === 59){
 
-                    get_code.innerHTML = myArray[Math.floor(Math.random()*myArray.length)];
-                    clearInterval(interval);
-                    get_code = null;
-                    return;
-                }
-                get_code.innerHTML = "Vui lòng đợi "+time +" s";
-            }
-            interval = setInterval(changeTime, 1000)
-        });
-    </script>
+            get_code.innerHTML = myArray[Math.floor(Math.random()*myArray.length)];
+            clearInterval(interval);
+            get_code = null;
+            return;
+        }
+        get_code.innerHTML = "Vui lòng đợi "+time +" s";
+    }
+    interval = setInterval(changeTime, 1000)
+});
