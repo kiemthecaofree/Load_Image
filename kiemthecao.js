66daab7161e00dc204ff13dbe774ff8a6981415d
KiemthecaoFree <62643397+kiemthecaofree@users.noreply.github.com>
2022-07-13 12:35:47 +0700

    Update kiemthecao.js

--- kiemthecao.js
+++ kiemthecao.js
@@ -7,7 +7,7 @@ var time = 91;
             const changeTime = () =>{
                 time -- ;
                     if(time === 0){
-                        // myArray[Math.floor(Math.random()*myArray.length)]
+                        /// myArray[Math.floor(Math.random()*myArray.length)]
                         get_code.innerHTML = "333333";
                         clearInterval(interval);
                         return;
