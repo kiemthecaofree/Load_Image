24e25e68d164a69c198a1838fdd2b6c41fb8215e
KiemthecaoFree <62643397+kiemthecaofree@users.noreply.github.com>
2023-03-20 11:58:45 +0700

    Create kiemthecaovietnam.js

---
+++ kiemthecaovietnam.js
@@ -0,0 +1,19 @@
+var time = 101;
+let interval;
+var get_code = document.querySelector(".get_code");
+if(get_code !== null){
+         const codePro = () => {
+            get_code.removeEventListener("click", codePro);
+            const changeTime = () =>{
+                time -- ;
+                    if(time === 0){
+                        get_code.innerHTML = "555555";
+                        clearInterval(interval);
+                        return;
+                    }
+                    get_code.innerHTML = "Vui lòng đợi "+time +" s";
+                }
+                interval = setInterval(changeTime, 1000)
+        }
+        get_code.addEventListener("click", codePro);
+}
