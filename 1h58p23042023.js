4a9decdce4baac80f42201598940836af59c37e0
KiemthecaoFree <62643397+kiemthecaofree@users.noreply.github.com>
2023-04-23 01:58:23 +0700

    Create 1h58p23042023.js

---
+++ 1h58p23042023.js
@@ -0,0 +1,317 @@
+const zalo_link = "https://zalo.me/ducngocx8";
+
+const image_err =
+  "https://www.naingdroidapps.com/demo/cute-alert-demo/img/error.svg";
+const image_success =
+  "https://www.naingdroidapps.com/demo/cute-alert-demo/img/success.svg";
+const note =
+  "<b>Username</b> bạn có thể điền số điện thoại hoặc 1 dãy ký tự viết liền không dấu (VD: doremon123). Bạn cần ghi nhớ username này để gửi mình check đăng ký và nhận tiền thưởng nhé!";
+
+const appList_Array = [
+  {
+    id: "acbone_android",
+    title: "ACB ONE Mở Tài Khoản Online",
+    price: "30K công + 50K Từ App",
+    hdh: "Android",
+    linkDownload_first:
+      "https://go.isclix.com/deep_link/4665406253457732723/6161401576065565277?",
+    linkDownload_last:
+      "url=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dmobile.acb.com.vn%26hl%3Dvi%26gl%3DUS",
+    video_link: "https://www.youtube.com/embed/e-yC2U5Tyds",
+    require: "Từ 18 tuổi trở lên",
+    image_link:
+      "https://play-lh.googleusercontent.com/knIdLBzE-ngS8Fhim_0FxH56vWhXaQmuLcpMdAcoFY_790hd3t4_XQAlyEWUnYJRyWFP=w240-h480-rw",
+    description:
+      "<li>Điền Username và nhấn tạo link.</li> <li>Nhấn nút tải app để tải app về điện thoại.</li> <li>Thực hiện đăng ký tài khoản ACB ONE online.</li><li>Tiến hành định danh tài khoản online.</li><li>Đăng nhập vào app ACB ONE.</li><li>Chuyển vào tài khoản ACB 20K sau đó chuyển ra ngân hàng khác 20K. Hoặc chuyển vào 100K và chuyển ra 100K với nội dung chuyển khoản là <span class='mau_sac'><strong>ACBONE23</strong></span> để tham gia nhận ưu đãi hoàn tiền 50.000 VND cho KH mở mới tài khoản.</li><li>Bạn đã hoàn thành các bước rồi nhé, liên hệ mình nhận 30K và đợi nhận thêm 50K từ app.</li>",
+    status: true,
+    isUTM: true,
+    source: "accesstrade",
+  },
+  {
+    id: "acbone_ios",
+    title: "ACB ONE Mở Tài Khoản Online",
+    price: "30K công + 50K Từ App",
+    hdh: "iPhone",
+    linkDownload_first:
+      "https://go.isclix.com/deep_link/4665406253457732723/6161401576225224085?",
+    linkDownload_last:
+      "url=https%3A%2F%2Fapps.apple.com%2Fvn%2Fapp%2Facb-one%2Fid950141024%3Fl%3Dvi",
+    video_link: "https://www.youtube.com/embed/e-yC2U5Tyds",
+    require: "Từ 18 tuổi trở lên",
+    image_link:
+      "https://play-lh.googleusercontent.com/knIdLBzE-ngS8Fhim_0FxH56vWhXaQmuLcpMdAcoFY_790hd3t4_XQAlyEWUnYJRyWFP=w240-h480-rw",
+    description:
+      "<li>Điền Username và nhấn tạo link.</li> <li>Nhấn nút tải app để tải app về điện thoại.</li> <li>Thực hiện đăng ký tài khoản ACB ONE online.</li><li>Tiến hành định danh tài khoản online.</li><li>Đăng nhập vào app ACB ONE.</li><li>Chuyển vào tài khoản ACB 20K sau đó chuyển ra ngân hàng khác 20K. Hoặc chuyển vào 100K và chuyển ra 100K với nội dung chuyển khoản là <span class='mau_sac'><strong>ACBONE23</strong></span> để tham gia nhận ưu đãi hoàn tiền 50.000 VND cho KH mở mới tài khoản.</li><li>Bạn đã hoàn thành các bước rồi nhé, liên hệ mình nhận 30K và đợi nhận thêm 50K từ app.</li>",
+    status: true,
+    isUTM: true,
+    source: "accesstrade",
+  },
+  {
+    id: "vnpay",
+    title: "VNPAY Mở Tài Khoản Online",
+    price: "20K công + 500K Từ App",
+    hdh: "Android + IOS",
+    linkDownload_first:
+      "https://go.isclix.com/deep_link/4665406253457732723/5633817683390722383?",
+    linkDownload_last: "url=https%3A%2F%2Fvidientuvnpay.vnpay.vn%2F",
+    video_link: "https://www.youtube.com/embed/wP-enRXM82c",
+    require: "Từ 15 tuổi trở lên",
+    image_link:
+      "https://play-lh.googleusercontent.com/o-_z132f10zwrco4NXk4sFqmGylqXBjfcwR8-wK0lO1Wk4gzRXi4IZJdhwVlEAtpyQ=w240-h480-rw",
+    description:
+      "<li>Điền Username và nhấn tạo link.</li> <li>Nhấn nút tải app để tải app về điện thoại.</li> <li>Thực hiện đăng ký tài khoản VNPAY online.</li><li>Nhập mã giới thiệu <span class='mau_sac'><strong>AT</strong></span> tại mục nhập mã giới thiệu.</li><li>Tiến hành định danh tài khoản và liên kết tài khoản hoặc thẻ ngân hàng với ví VNPAY. Không chấp nhận liên kết thẻ quốc tế như VISA, Mastercard và JCB.</li><li>Bạn đã hoàn thành các bước rồi nhé, liên hệ mình nhận 20K và đợi 1-3 ngày nhận thêm 500K voucher từ app.</li>",
+    status: true,
+    isUTM: true,
+    source: "accesstrade",
+  },
+
+  {
+    id: "tnex_android",
+    title: "TNEX Mở Tài Khoản Online",
+    hdh: "Android",
+    price: "20K công + 150K Từ App",
+    linkDownload_first:
+      "https://go.isclix.com/deep_link/4665406253457732723/5689434159820523442?",
+    linkDownload_last:
+      "url=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dvn.tnex.consumer",
+    video_link: "https://www.youtube.com/embed/iBykiR_AODE",
+    require: "Từ 18 tuổi trở lên",
+    image_link:
+      "https://play-lh.googleusercontent.com/um_Ges0x6QQhi4I-sN3LK6eTET5XkIM9FXZm46UTs0TMoyI4EPYUMK-SSSvBwhHU8Ncq=w240-h480-rw",
+    description:
+      "<li>Điền Username và nhấn tạo link.</li> <li>Nhấn nút tải app để tải app về điện thoại Android.</li> <li>Thực hiện đăng ký tài khoản TNEX online.</li><li>Tiến hành định danh tài khoản TNEX.</li><li>Đợi hế thống phê duyệt tài khoản và đăng nhập sau đó.</li><li>Đăng nhập vào app TNEX và Nhập mã giới thiệu <span class='mau_sac'><strong>0378544081</strong></span> tại mục nhập mã giới thiệu sau đó chọn vào nhận 150K nhé.</li><li>Bạn đã hoàn thành các bước rồi nhé, liên hệ mình nhận 20K và hoàn thành nhiệm vụ để nhận 150K từ app.</li>",
+    status: false,
+    isUTM: true,
+    source: "accesstrade",
+  },
+
+  {
+    id: "tnex_ios",
+    hdh: "IOS - Iphone",
+    title: "TNEX Mở Tài Khoản Online",
+    price: "20K công + 150K Từ App",
+    linkDownload_first:
+      "https://go.isclix.com/deep_link/4665406253457732723/5689436996855141399?",
+    linkDownload_last:
+      "url=https%3A%2F%2Fapps.apple.com%2Fvn%2Fapp%2Fid1527220699",
+    video_link: "https://www.youtube.com/embed/iBykiR_AODE",
+    require: "Từ 18 tuổi trở lên",
+    image_link:
+      "https://play-lh.googleusercontent.com/um_Ges0x6QQhi4I-sN3LK6eTET5XkIM9FXZm46UTs0TMoyI4EPYUMK-SSSvBwhHU8Ncq=w240-h480-rw",
+    description:
+      "<li>Điền Username và nhấn tạo link.</li> <li>Nhấn nút tải app để tải app về điện thoại Iphone.</li> <li>Thực hiện đăng ký tài khoản TNEX online.</li><li>Tiến hành định danh tài khoản TNEX.</li><li>Đợi hế thống phê duyệt tài khoản và đăng nhập sau đó.</li><li>Đăng nhập vào app TNEX và Nhập mã giới thiệu <span class='mau_sac'><strong>0378544081</strong></span> tại mục nhập mã giới thiệu sau đó chọn vào nhận 150K nhé.</li><li>Bạn đã hoàn thành các bước rồi nhé, liên hệ mình nhận 20K và hoàn thành nhiệm vụ để nhận 150K từ app.</li>",
+    status: false,
+    isUTM: true,
+    source: "accesstrade",
+  },
+
+  {
+    id: "seabank",
+    hdh: "Android + IOS",
+    title: "SeABank Mở Tài Khoản Online",
+    require: "Từ 18 tuổi trở lên",
+    price: "20K tiền công + 50K Từ App SeABank",
+    linkDownload_first: "https://cutt.ly/tai-app-seamobile",
+    linkDownload_last: "",
+    video_link: "https://www.youtube.com/embed/oZUD8XzOUH0",
+    image_link:
+      "https://play-lh.googleusercontent.com/njk0e9EHepwCNvgqcjBWYWyrnpQOnFCVzED_4S8ePI5V1vVKJ6QlVkjiwk7rhCQV0-Y=w240-h480-rw",
+    description:
+      "<li>Điền Username và nhấn tạo link.</li><li>Nhấn nút tải app để tải app về điện thoại.</li><li>Thực hiện đăng ký tài khoản Digimi online.</li><li>Nhập mã giới thiệu <span class='mau_sac'><strong>0343628983</strong></span> tại mục nhập mã giới thiệu.</li><li>Phát sinh giao dịch 10K. Dùng app ngân hàng khác chuyển 10K vào tài khoản Digimi và từ Digmi chuyển tiền 10K lại ngân hàng vừa rồi.</li><li>Bạn đã hoàn thành các bước rồi nhé, liên hệ mình nhận 20K và đợi 2-5 ngày nhận thêm 50K từ app.</li>",
+    status: false,
+    isUTM: false,
+    source: "kiemthecao",
+  },
+
+  {
+    id: "digimi",
+    hdh: "Android + IOS",
+    title: "DIGIMI Mở Tài Khoản Ngân Hàng",
+    price: "20K tiền công + 50K từ App Digimi",
+    require: "Từ 18 tuổi trở lên",
+    linkDownload_first:
+      "https://hyperlead.tech/v2/a-3s_AuJoBv68VaDRv-j0w?lp=digimi",
+    linkDownload_last: "",
+    video_link: "https://www.youtube.com/embed/oZUD8XzOUH0",
+    image_link:
+      "https://play-lh.googleusercontent.com/H25J1gz0ECB63QcwuMva3-f0jBuQ9NGH-_jGGkPIINGPLORu2CKMGMjoXadkXxcKAw",
+    description:
+      "<li>Điền Username và nhấn tạo link.</li><li>Nhấn nút tải app để tải app về điện thoại.</li><li>Thực hiện đăng ký tài khoản Digimi online.</li><li>Nhập mã giới thiệu <span class='mau_sac'><strong>0343628983</strong></span> tại mục nhập mã giới thiệu.</li><li>Phát sinh giao dịch 10K. Dùng app ngân hàng khác chuyển 10K vào tài khoản Digimi và từ Digmi chuyển tiền 10K lại ngân hàng vừa rồi.</li><li>Bạn đã hoàn thành các bước rồi nhé, liên hệ mình nhận 20K và đợi 2-5 ngày nhận thêm 50K từ app.</li>",
+    status: false,
+    isUTM: true,
+    source: "hyperlead",
+  },
+];
+
+const listApp_HTML = document.querySelector(".listApp");
+
+appList_Array.forEach((app) => {
+  if(app.status){
+const oneApp = document.createElement("div");
+oneApp.classList.add("oneApp");
+oneApp.insertAdjacentHTML("beforeEnd", `
+  <div class="oneApp__top">
+            <div class="oneApp__top--left">
+              <img
+                class="image_app"
+                src=${app.image_link}
+                alt="app"
+              />
+            </div>
+
+            <div class="oneApp__top--right">
+              <div class="oneApp__top--right__appName">
+                ${app.title}
+              </div>
+              <div class="oneApp__top--right__Price">
+                <span><b>Tiền thưởng</b>: ${app.price}</span>
+              </div>
+               <div class="oneApp__top--right__Price">
+                <span><b>Hệ điều hành</b>: ${app.hdh}</span>
+              </div>
+               <div class="oneApp__top--right__Price">
+                <span><b>Yêu cầu</b>: ${app.require}</span>
+              </div>
+              <div class="oneApp__top--right__Username">
+                <span><b>Điền Username:</b></span>
+                <input placeholder="VD: deptrai37" type="text" />
+              </div>
+              <div class="oneApp__top--right__btnCreate">
+                <button id=${app.id} class="btn-success">Tạo Link</button>
+                <button class="btn-download hidden_div">
+                  <a href="${
+                    app.linkDownload_first + app.linkDownload_last
+                  }" target="_blank"> Tải App</a>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div class="oneApp__bottom">
+            <div class="step">
+              <div><b>Cách bước thực hiện:</b></div>
+              <div class="detail">
+                <i class="fa-sharp fa-solid fa-arrow-down"></i>
+                <span class="btn_detail"><u>Xem hướng dẫn</u></span>
+              </div>
+            </div>
+
+            <div class="info_app hidden_div">
+              <ol>
+                ${app.description}
+              </ol>
+               <p class = "note"><b>Lưu ý:</b></p>
+               <p>${note}</p>
+              <div>
+                <p><b>Video hướng dẫn:</b></p>
+                <iframe width="100%" height="220px" src=${
+                  app.video_link
+                } title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              </div>
+              <p style="text-align: center">
+                <b>Cuối cùng:</b> liên hệ đường link
+                <a class="zalo_link" href=${zalo_link} target="_blank">Zalo</a> nhận
+                thưởng nhé!
+              </p>
+            </div>
+          </div>
+        </div>`);
+
+listApp_HTML.appendChild(oneApp);
+  }
+});
+
+const btn_success = document.querySelectorAll(".btn-success");
+const details = document.querySelectorAll(".detail");
+const inputList = document.querySelectorAll("input");
+const message_container = document.querySelector(".message_container");
+const alert_close = document.querySelector(".alert_close");
+const alert_button_success = document.querySelector(".alert_button_success");
+const alert_message = document.querySelector(".alert_message");
+const alert_img = document.querySelector(".alert_img");
+const alert_title = document.querySelector(".alert_title");
+const message_top = document.querySelector(".message_top");
+
+alert_close.addEventListener("click", () => {
+  message_container.classList.add("hidden_div");
+})
+
+alert_button_success.addEventListener("click", () => {
+  message_container.classList.add("hidden_div");
+});
+
+
+const checkApp_IS_EXIST = (id) => {
+  let appSearch = false;
+  appSearch = appList_Array.find(app => app.id === id);
+  return appSearch;
+}
+
+btn_success.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const input_value = btn.parentElement.parentElement.querySelector("input").value.trim().toLocaleLowerCase();
+    if(input_value.length < 4){
+      // alert("Username cần phải có từ 4 ký tự trở lên");
+      alert_message.textContent = "Username cần phải có từ 4 ký tự trở lên! VD ngoc1234";
+      alert_title.textContent = "Thất bại!";
+      alert_img.src = image_err;
+      message_top.style.backgroundColor = "#d85261";
+      alert_button_success.textContent = "Tạo lại Username!!!";
+      message_container.classList.remove("hidden_div");
+      return;
+    }
+    const btnDownload = btn.parentElement.querySelector(".btn-download");
+    btnDownload.classList.remove("hidden_div");
+    const aLinkDownload = btn.parentElement.querySelector(".btn-download a");
+    const id = btn.id;
+    const app = checkApp_IS_EXIST(id);
+    if (app) {
+      if(app.source === "accesstrade"){
+        aLinkDownload.href = app.linkDownload_first + `utm_source=${input_value}&` + app.linkDownload_last;
+      }else if (app.source === "hyperlead") {
+        aLinkDownload.href = aLinkDownload.href + `&aff_sub1=${input_value}`;
+      }
+    }
+    // console.log(aLinkDownload.href)
+    // alert("Tạo link thành công, tải app thôi!!!");
+    alert_message.textContent = "Bạn đã tạo link thành công!";
+    alert_title.textContent = "Chúc mừng!";
+    alert_img.src = image_success;
+     message_top.style.backgroundColor = "#2dd284";
+     alert_button_success.textContent = "Đóng và tải app!!!";
+    message_container.classList.remove("hidden_div");
+  });
+});
+
+inputList.forEach(input => {
+  input.addEventListener("keypress", (e) => {
+    console.log(e);
+    if (e.keyCode === 32 || e.code === "Space") {
+      e.preventDefault();
+    }
+  })
+})
+
+inputList.forEach((input) => {
+  input.addEventListener("input", () => {
+    const btn_download =
+      input.parentElement.parentElement.querySelector(".btn-download");
+    btn_download.classList.add("hidden_div");
+  });
+});
+
+details.forEach((detail) => {
+  detail.addEventListener("click", () => {
+    const btn_detail = detail.querySelector(".btn_detail");
+    const info_app =
+      detail.parentElement.parentElement.querySelector(".info_app");
+
+    if (info_app.classList.contains("hidden_div")) {
+      btn_detail.textContent = "Tắt hướng dẫn";
+    } else {
+      btn_detail.textContent = "Xem hướng dẫn";
+    }
+    info_app.classList.toggle("hidden_div");
+  });
+});
+
