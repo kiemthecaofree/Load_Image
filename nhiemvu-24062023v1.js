62e8125d316e60d112a5d093bd498cfc27ecad16
KiemthecaoFree <62643397+kiemthecaofree@users.noreply.github.com>
2023-06-25 05:24:14 +0700

    Update nhiemvu-24062023v1.js

--- nhiemvu-24062023v1.js
+++ nhiemvu-24062023v1.js
@@ -1,80 +1,183 @@
-const server_select_div = document.querySelector("#select_server");
-const website_input = document.querySelector("#website_input");
-const btn_submit = document.querySelector(".btn_submit");
-const txt_code = document.querySelector(".txt_code");
-const btn_copy = document.querySelector(".btn_copy");
-const message_container = document.querySelector(".message_container");
-const alert_close = document.querySelector(".alert_close");
-const alert_button_success = document.querySelector(".alert_button_success");
-const alert_message = document.querySelector(".alert_message");
-
-const domain_api = [
-  {
-    website: "lamngay.com",
-    api_link: "https://cdn-chatgpt.com/api/mission-code",
-  },
-  {
-    website: "lamngay.vn",
-    api_link: "https://cdn-chatgpt.com/api/mission-code",
-  },
+var _0x5519 = [
+  "\x23\x73\x65\x6C\x65\x63\x74\x5F\x73\x65\x72\x76\x65\x72",
+  "\x71\x75\x65\x72\x79\x53\x65\x6C\x65\x63\x74\x6F\x72",
+  "\x23\x77\x65\x62\x73\x69\x74\x65\x5F\x69\x6E\x70\x75\x74",
+  "\x2E\x62\x74\x6E\x5F\x73\x75\x62\x6D\x69\x74",
+  "\x2E\x74\x78\x74\x5F\x63\x6F\x64\x65",
+  "\x2E\x62\x74\x6E\x5F\x63\x6F\x70\x79",
+  "\x2E\x6D\x65\x73\x73\x61\x67\x65\x5F\x63\x6F\x6E\x74\x61\x69\x6E\x65\x72",
+  "\x2E\x61\x6C\x65\x72\x74\x5F\x63\x6C\x6F\x73\x65",
+  "\x2E\x61\x6C\x65\x72\x74\x5F\x62\x75\x74\x74\x6F\x6E\x5F\x73\x75\x63\x63\x65\x73\x73",
+  "\x2E\x61\x6C\x65\x72\x74\x5F\x6D\x65\x73\x73\x61\x67\x65",
+  "\x23\x73\x74\x65\x70\x5F\x32",
+  "\x2E\x71\x75\x69\x7A\x61\x70\x70\x5F\x6D\x65\x73\x73\x61\x67\x65",
+  "\x6C\x61\x6D\x6E\x67\x61\x79\x2E\x63\x6F\x6D",
+  "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x63\x64\x6E\x2D\x63\x68\x61\x74\x67\x70\x74\x2E\x63\x6F\x6D\x2F\x61\x70\x69\x2F\x6D\x69\x73\x73\x69\x6F\x6E\x2D\x63\x6F\x64\x65",
+  "\x6C\x61\x6D\x6E\x67\x61\x79\x2E\x76\x6E",
+  "\x71\x75\x69\x7A\x61\x70\x70",
+  "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x6C\x61\x79\x6D\x61\x6E\x67\x61\x79\x2E\x63\x6F\x6D\x2F\x70\x75\x62\x6C\x69\x73\x68\x65\x72\x3F",
+  "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x6C\x61\x79\x6D\x61\x6E\x67\x61\x79\x2E\x63\x6F\x6D\x2F\x71\x75\x65\x3F\x71\x3D\x73\x74\x61\x74\x75\x73\x2C\x61\x7A\x61\x75\x74\x68\x2C\x71\x2C\x74\x2C\x7A\x26\x66\x69\x6C\x74\x65\x72\x3D\x63\x6F\x6E\x6E\x65\x63\x74\x69\x6F\x6E",
+  "\x6A\x73\x6F\x6E",
+  "\x74\x68\x65\x6E",
+  "\x50\x4F\x53\x54",
+  "\x61\x70\x70\x6C\x69\x63\x61\x74\x69\x6F\x6E\x2E\x6A\x73\x6F\x6E",
+  "\x61\x70\x70\x6C\x69\x63\x61\x74\x69\x6F\x6E\x2F\x6A\x73\x6F\x6E",
+  "\x73\x74\x72\x69\x6E\x67\x69\x66\x79",
+  "\x47\x45\x54",
+  "\x63\x6C\x69\x63\x6B",
+  "\x74\x65\x78\x74\x43\x6F\x6E\x74\x65\x6E\x74",
+  "\x5F\x5F\x5F\x5F\x5F\x5F",
+  "\x43\x4F\x50\x59",
+  "\x76\x61\x6C\x75\x65",
+  "\x74\x72\x69\x6D",
+  "\x77\x65\x62\x73\x69\x74\x65",
+  "\x66\x69\x6E\x64",
+  "\x6C\x65\x6E\x67\x74\x68",
+  "\x42\u1EA1\x6E\x20\x63\x68\u01B0\x61\x20\u0111\x69\u1EC1\x6E\x20\x77\x65\x62\x73\x69\x74\x65\x2E\x20\x56\x75\x69\x20\x6C\xF2\x6E\x67\x20\x6E\x68\u1EAD\x70\x20\x77\x65\x62\x73\x69\x74\x65\x20\x76\xE0\x20\x74\x68\u1EED\x20\x6C\u1EA1\x69\x21",
+  "\x68\x69\x64\x64\x65\x6E\x5F\x64\x69\x76",
+  "\x72\x65\x6D\x6F\x76\x65",
+  "\x63\x6C\x61\x73\x73\x4C\x69\x73\x74",
+  "",
+  "\x61\x70\x69\x5F\x6C\x69\x6E\x6B",
+  "\x61\x70\x69\x5F\x6C\x69\x6E\x6B\x5F\x70\x61\x72\x61\x6D",
+  "\x73\x74\x61\x74\x75\x73",
+  "\x53\x65\x72\x76\x65\x72\x20\u0111\x61\x6E\x67\x20\x6C\u1ED7\x69\x21\x20\x42\u1EA1\x6E\x20\x68\xE3\x79\x20\x6C\x69\xEA\x6E\x20\x68\u1EC7\x20\x5A\x61\x6C\x6F\x2F\x59\x6F\x75\x74\x75\x62\x65\x20\u0111\u1EC3\x20\x62\xE1\x6F\x20\x63\xE1\x6F\x20\x73\u1EF1\x20\x63\u1ED1\x20\x6E\x68\xE9\x21",
+  "\x61\x7A\x61\x75\x74\x68\x3D",
+  "\x26\x71\x3D",
+  "\x26\x74\x3D",
+  "\x26\x6F\x70\x61\x3D\x31\x32\x33\x26\x7A\x3D\x31\x32\x33",
+  "\x73\x75\x63\x63\x65\x73\x73",
+  "\x64\x61\x74\x61",
+  "\x70\x61\x73\x73\x77\x6F\x72\x64",
+  "\x43\xF3\x20\x74\x68\u1EC3\x20\x77\x65\x62\x73\x69\x74\x65\x20\x62\u1EA1\x6E\x20\x6E\x68\u1EAD\x70\x20\x63\x68\u01B0\x61\x20\x63\x68\xED\x6E\x68\x20\x78\xE1\x63\x2E\x20\x56\x75\x69\x20\x6C\xF2\x6E\x67\x20\x74\x68\u1EED\x20\x6C\u1EA1\x69\x21",
+  "\x53\x65\x72\x76\x65\x72\x20\x6B\x68\xF4\x6E\x67\x20\x74\xEC\x6D\x20\x74\x68\u1EA5\x79\x2E\x20\x56\x75\x69\x20\x6C\xF2\x6E\x67\x20\x74\x68\u1EED\x20\x6C\u1EA1\x69\x21",
+  "\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72",
+  "\x77\x72\x69\x74\x65\x54\x65\x78\x74",
+  "\x63\x6C\x69\x70\x62\x6F\x61\x72\x64",
+  "\u0110\xC3\x20\x43\x4F\x50\x59",
+  "\x61\x64\x64",
+  "\x63\x68\x61\x6E\x67\x65",
 ];
-
-const getCodeVIP = (api_domain, website_input_value) =>
-  fetch(api_domain, {
-    method: "POST",
+const server_select_div = document[_0x5519[1]](_0x5519[0]);
+const website_input = document[_0x5519[1]](_0x5519[2]);
+const btn_submit = document[_0x5519[1]](_0x5519[3]);
+const txt_code = document[_0x5519[1]](_0x5519[4]);
+const btn_copy = document[_0x5519[1]](_0x5519[5]);
+const message_container = document[_0x5519[1]](_0x5519[6]);
+const alert_close = document[_0x5519[1]](_0x5519[7]);
+const alert_button_success = document[_0x5519[1]](_0x5519[8]);
+const alert_message = document[_0x5519[1]](_0x5519[9]);
+const step_2 = document[_0x5519[1]](_0x5519[10]);
+const quizapp_message = document[_0x5519[1]](_0x5519[11]);
+const domain_api = [
+  { website: _0x5519[12], api_link: _0x5519[13] },
+  { website: _0x5519[14], api_link: _0x5519[13] },
+  { website: _0x5519[15], api_link: _0x5519[16], api_link_param: _0x5519[17] },
+];
+const getCodeVIPLamNgayCom = (_0x37cexe, _0x37cexf) => {
+  return fetch(_0x37cexe, {
+    method: _0x5519[20],
     headers: {
-      headers: "application.json",
-      "Content-Type": "application/json",
+      headers: _0x5519[21],
+      "\x43\x6F\x6E\x74\x65\x6E\x74\x2D\x54\x79\x70\x65": _0x5519[22],
     },
-    body: JSON.stringify({
-      hostname: website_input_value,
-    }),
-  }).then((res) => {
-    return res.json();
+    body: JSON[_0x5519[23]]({ hostname: _0x37cexf }),
+  })[_0x5519[19]]((_0x37cex10) => {
+    return _0x37cex10[_0x5519[18]]();
   });
-
-btn_submit.addEventListener("click", async () => {
-  txt_code.textContent = "______";
-  btn_copy.textContent = "COPY";
-  const server_select_value = server_select_div.value;
-  const website_input_value = website_input.value.trim();
-  if (website_input_value.length === 0) {
-    alert_message.textContent =
-      "Bạn chưa điền website. Vui lòng nhập website và thử lại!";
-    message_container.classList.remove("hidden_div");
-    return;
-  }
-  const domain = domain_api.find(
-    (element) => element.website === server_select_value
-  );
-  if (domain) {
-    const result = await getCodeVIP(domain.api_link, website_input_value);
-    if (result.success) {
-      txt_code.textContent = result.data;
-      website_input.value = "";
+};
+const getParamQuizApp = (_0x37cexe) => {
+  return fetch(_0x37cexe, { method: _0x5519[24] })
+    [_0x5519[19]]((_0x37cex10) => {
+      return _0x37cex10[_0x5519[18]]();
+    })
+    [_0x5519[19]]((_0x37cex12) => {
+      return _0x37cex12;
+    });
+};
+const getCodeVIPQuizApp = (_0x37cexe) => {
+  return fetch(_0x37cexe, { method: _0x5519[24] })
+    [_0x5519[19]]((_0x37cex10) => {
+      return _0x37cex10[_0x5519[18]]();
+    })
+    [_0x5519[19]]((_0x37cex12) => {
+      return _0x37cex12;
+    });
+};
+btn_submit[_0x5519[52]](_0x5519[25], async () => {
+  txt_code[_0x5519[26]] = _0x5519[27];
+  btn_copy[_0x5519[26]] = _0x5519[28];
+  const _0x37cex14 = server_select_div[_0x5519[29]];
+  const _0x37cexf = website_input[_0x5519[29]][_0x5519[30]]();
+  const _0x37cex15 = domain_api[_0x5519[32]]((_0x37cex16) => {
+    return _0x37cex16[_0x5519[31]] === _0x37cex14;
+  });
+  if (_0x37cex15) {
+    if (
+      _0x37cexf[_0x5519[33]] === 0 &&
+      _0x37cex15[_0x5519[31]] !== _0x5519[15]
+    ) {
+      alert_message[_0x5519[26]] = _0x5519[34];
+      message_container[_0x5519[37]][_0x5519[36]](_0x5519[35]);
+      return;
+    }
+    let _0x37cex17 = _0x5519[38];
+    let _0x37cex18 = {};
+    let _0x37cex19 = false;
+    let _0x37cex1a = false;
+    if (_0x37cex15[_0x5519[31]] == _0x5519[12]) {
+      _0x37cex17 = _0x37cex15[_0x5519[39]];
+      _0x37cex19 = await getCodeVIPLamNgayCom(_0x37cex17, _0x37cexf);
     } else {
-      alert_message.textContent =
-        "Có thể website bạn nhập chưa chính xác. Vui lòng thử lại!";
-      message_container.classList.remove("hidden_div");
+      if (_0x37cex15[_0x5519[31]] == _0x5519[15]) {
+        const _0x37cex1b = _0x37cex15[_0x5519[40]];
+        _0x37cex18 = await getParamQuizApp(_0x37cex1b);
+        const { azauth, q, t } = _0x37cex18;
+        _0x37cex1a = _0x37cex18[_0x5519[41]];
+        if (!_0x37cex1a) {
+          alert_message[_0x5519[26]] = _0x5519[42];
+          message_container[_0x5519[37]][_0x5519[36]](_0x5519[35]);
+        }
+        _0x37cex17 = `${_0x5519[38]}${_0x37cex15[_0x5519[39]]}${
+          _0x5519[43]
+        }${azauth}${_0x5519[44]}${q}${_0x5519[45]}${t}${_0x5519[46]}`;
+        _0x37cex19 = await getCodeVIPQuizApp(_0x37cex17);
+        _0x37cex1a = _0x37cex19[_0x5519[41]];
+      }
+    }
+    if (_0x37cex19[_0x5519[47]] || _0x37cex1a) {
+      txt_code[_0x5519[26]] =
+        _0x37cex19[_0x5519[48]] || _0x37cex19[_0x5519[49]];
+      website_input[_0x5519[29]] = _0x5519[38];
+    } else {
+      alert_message[_0x5519[26]] = _0x5519[50];
+      message_container[_0x5519[37]][_0x5519[36]](_0x5519[35]);
     }
   } else {
-     alert_message.textContent = "Server không tìm thấy. Vui lòng thử lại!";
-     message_container.classList.remove("hidden_div");
+    alert_message[_0x5519[26]] = _0x5519[51];
+    message_container[_0x5519[37]][_0x5519[36]](_0x5519[35]);
   }
 });
-
-btn_copy.addEventListener("click", () => {
-  const value_copy = txt_code.textContent.trim();
-  if (value_copy.length > 0) {
-    navigator.clipboard.writeText(value_copy);
-    btn_copy.textContent = "ĐÃ COPY";
+btn_copy[_0x5519[52]](_0x5519[25], () => {
+  const _0x37cex1c = txt_code[_0x5519[26]][_0x5519[30]]();
+  if (_0x37cex1c[_0x5519[33]] > 0) {
+    navigator[_0x5519[54]][_0x5519[53]](_0x37cex1c);
+    btn_copy[_0x5519[26]] = _0x5519[55];
   }
 });
-
-alert_close.addEventListener("click", () => {
-  message_container.classList.add("hidden_div");
+alert_close[_0x5519[52]](_0x5519[25], () => {
+  message_container[_0x5519[37]][_0x5519[56]](_0x5519[35]);
 });
-
-alert_button_success.addEventListener("click", () => {
-  message_container.classList.add("hidden_div");
+alert_button_success[_0x5519[52]](_0x5519[25], () => {
+  message_container[_0x5519[37]][_0x5519[56]](_0x5519[35]);
+});
+server_select_div[_0x5519[52]](_0x5519[57], () => {
+  const _0x37cex14 = server_select_div[_0x5519[29]];
+  if (_0x37cex14 == _0x5519[15]) {
+    step_2[_0x5519[37]][_0x5519[56]](_0x5519[35]);
+    quizapp_message[_0x5519[37]][_0x5519[36]](_0x5519[35]);
+  } else {
+    step_2[_0x5519[37]][_0x5519[36]](_0x5519[35]);
+    quizapp_message[_0x5519[37]][_0x5519[56]](_0x5519[35]);
+  }
 });
