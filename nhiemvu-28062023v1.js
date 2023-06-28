19800decd427c766b61e29cc75ee401ad22a801f
KiemthecaoFree <62643397+kiemthecaofree@users.noreply.github.com>
2023-06-28 19:22:39 +0700

    Create nhiemvu-28062023v1.js

---
+++ nhiemvu-28062023v1.js
@@ -0,0 +1,179 @@
+var _0xb455 = [
+  "\x23\x73\x65\x6C\x65\x63\x74\x5F\x73\x65\x72\x76\x65\x72",
+  "\x71\x75\x65\x72\x79\x53\x65\x6C\x65\x63\x74\x6F\x72",
+  "\x23\x77\x65\x62\x73\x69\x74\x65\x5F\x69\x6E\x70\x75\x74",
+  "\x2E\x62\x74\x6E\x5F\x73\x75\x62\x6D\x69\x74",
+  "\x2E\x74\x78\x74\x5F\x63\x6F\x64\x65",
+  "\x2E\x62\x74\x6E\x5F\x63\x6F\x70\x79",
+  "\x2E\x6D\x65\x73\x73\x61\x67\x65\x5F\x63\x6F\x6E\x74\x61\x69\x6E\x65\x72",
+  "\x2E\x61\x6C\x65\x72\x74\x5F\x63\x6C\x6F\x73\x65",
+  "\x2E\x61\x6C\x65\x72\x74\x5F\x62\x75\x74\x74\x6F\x6E\x5F\x73\x75\x63\x63\x65\x73\x73",
+  "\x2E\x61\x6C\x65\x72\x74\x5F\x6D\x65\x73\x73\x61\x67\x65",
+  "\x23\x73\x74\x65\x70\x5F\x32",
+  "\x2E\x71\x75\x69\x7A\x61\x70\x70\x5F\x6D\x65\x73\x73\x61\x67\x65",
+  "\x6C\x61\x6D\x6E\x67\x61\x79\x2E\x63\x6F\x6D",
+  "\x6C\x61\x6D\x6E\x67\x61\x79\x2E\x76\x6E",
+  "\x71\x75\x69\x7A\x61\x70\x70",
+  "\x6A\x73\x6F\x6E",
+  "\x74\x68\x65\x6E",
+  "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x63\x6F\x64\x65\x2E\x6D\x61\x67\x69\x6F\x69\x74\x68\x69\x65\x75\x2E\x6E\x65\x74\x2F\x6C\x61\x6D\x6E\x67\x61\x79\x63\x6F\x6D",
+  "\x50\x4F\x53\x54",
+  "\x61\x70\x70\x6C\x69\x63\x61\x74\x69\x6F\x6E\x2E\x6A\x73\x6F\x6E",
+  "\x61\x70\x70\x6C\x69\x63\x61\x74\x69\x6F\x6E\x2F\x6A\x73\x6F\x6E",
+  "\x73\x74\x72\x69\x6E\x67\x69\x66\x79",
+  "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x63\x6F\x64\x65\x2E\x6D\x61\x67\x69\x6F\x69\x74\x68\x69\x65\x75\x2E\x6E\x65\x74\x2F\x71\x75\x69\x7A\x61\x70\x70",
+  "\x63\x6C\x69\x63\x6B",
+  "\x74\x65\x78\x74\x43\x6F\x6E\x74\x65\x6E\x74",
+  "\x5F\x5F\x5F\x5F\x5F\x5F",
+  "\x43\x4F\x50\x59",
+  "\x76\x61\x6C\x75\x65",
+  "\x74\x72\x69\x6D",
+  "\x66\x69\x6E\x64\x49\x6E\x64\x65\x78",
+  "\x6C\x65\x6E\x67\x74\x68",
+  "\x42\u1EA1\x6E\x20\x63\x68\u01B0\x61\x20\u0111\x69\u1EC1\x6E\x20\x77\x65\x62\x73\x69\x74\x65\x2E\x20\x56\x75\x69\x20\x6C\xF2\x6E\x67\x20\x6E\x68\u1EAD\x70\x20\x77\x65\x62\x73\x69\x74\x65\x20\x76\xE0\x20\x74\x68\u1EED\x20\x6C\u1EA1\x69\x21",
+  "\x68\x69\x64\x64\x65\x6E\x5F\x64\x69\x76",
+  "\x72\x65\x6D\x6F\x76\x65",
+  "\x63\x6C\x61\x73\x73\x4C\x69\x73\x74",
+  "",
+  "\x53\x65\x72\x76\x65\x72\x20\x6B\x68\xF4\x6E\x67\x20\x74\xEC\x6D\x20\x74\x68\u1EA5\x79\x2E\x20\x56\x75\x69\x20\x6C\xF2\x6E\x67\x20\x74\x68\u1EED\x20\x6C\u1EA1\x69\x21",
+  "\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72",
+  "\x77\x72\x69\x74\x65\x54\x65\x78\x74",
+  "\x63\x6C\x69\x70\x62\x6F\x61\x72\x64",
+  "\u0110\xC3\x20\x43\x4F\x50\x59",
+  "\x61\x64\x64",
+  "\x63\x68\x61\x6E\x67\x65",
+  "\x23\x74\x69\x6D\x65\x5F\x6E\x75\x6D\x62\x65\x72",
+  "\x23\x74\x69\x6D\x65\x5F\x74\x65\x78\x74\x5F\x74\x6F\x70",
+  "\x23\x74\x69\x6D\x65\x5F\x74\x65\x78\x74\x5F\x6C\x61\x73\x74",
+  "\x62\x6F\x72\x64\x65\x72",
+  "\x73\x74\x79\x6C\x65",
+  "\x31\x70\x78\x20\x73\x6F\x6C\x69\x64\x20\x62\x6C\x75\x65",
+  "\x73\x61\x75\x20\u0111\xF3\x20\x6D\u1EDB\x69\x20\x6E\x68\u1EAD\x70\x20\x6D\xE3\x20\x63\x6F\x64\x65\x21",
+  "\x73",
+  "\x20\x48\u1EBF\x74\x20\x67\x69\u1EDD\x21\x20\x42\u1EA1\x6E\x20\x63\xF3\x20\x74\x68\u1EC3\x20\x6E\x68\u1EAD\x70\x20\x6D\xE3\x20\x63\x6F\x64\x65\x20\x72\u1ED3\x69\x2E",
+  "\x6E\x6F\x6E\x65",
+];
+const a = document[_0xb455[1]](_0xb455[0]);
+const b = document[_0xb455[1]](_0xb455[2]);
+const c = document[_0xb455[1]](_0xb455[3]);
+const d = document[_0xb455[1]](_0xb455[4]);
+const e = document[_0xb455[1]](_0xb455[5]);
+const f = document[_0xb455[1]](_0xb455[6]);
+const g = document[_0xb455[1]](_0xb455[7]);
+const h = document[_0xb455[1]](_0xb455[8]);
+const i = document[_0xb455[1]](_0xb455[9]);
+const k = document[_0xb455[1]](_0xb455[10]);
+const l = document[_0xb455[1]](_0xb455[11]);
+let interval;
+const m = [_0xb455[12], _0xb455[13], _0xb455[14]];
+const p = (_0x739cxf, _0x739cx10) => {
+  return fetch(`${_0xb455[17]}`, {
+    method: _0xb455[18],
+    headers: {
+      headers: _0xb455[19],
+      "\x43\x6F\x6E\x74\x65\x6E\x74\x2D\x54\x79\x70\x65": _0xb455[20],
+    },
+    body: JSON[_0xb455[21]]({ hostname: _0x739cx10, domain_id: _0x739cxf }),
+  })[_0xb455[16]]((_0x739cx11) => {
+    return _0x739cx11[_0xb455[15]]();
+  });
+};
+const q = (_0x739cxf) => {
+  return fetch(`${_0xb455[22]}`, {
+    method: _0xb455[18],
+    headers: {
+      headers: _0xb455[19],
+      "\x43\x6F\x6E\x74\x65\x6E\x74\x2D\x54\x79\x70\x65": _0xb455[20],
+    },
+    body: JSON[_0xb455[21]]({ domain_id: _0x739cxf }),
+  })
+    [_0xb455[16]]((_0x739cx11) => {
+      return _0x739cx11[_0xb455[15]]();
+    })
+    [_0xb455[16]]((_0x739cx13) => {
+      return _0x739cx13;
+    });
+};
+c[_0xb455[37]](_0xb455[23], async () => {
+  d[_0xb455[24]] = _0xb455[25];
+  e[_0xb455[24]] = _0xb455[26];
+  const _0x739cx14 = a[_0xb455[27]];
+  const _0x739cx10 = b[_0xb455[27]][_0xb455[28]]();
+  const _0x739cx15 = m[_0xb455[29]]((_0x739cx16) => {
+    return _0x739cx16 === _0x739cx14;
+  });
+  if (_0x739cx15 !== -1) {
+    if (
+      _0x739cx10[_0xb455[30]] === 0 &&
+      m[_0x739cx15] !== _0xb455[14]
+    ) {
+      i[_0xb455[24]] = _0xb455[31];
+      f[_0xb455[34]][_0xb455[33]](_0xb455[32]);
+      return;
+    }
+    if (m[_0x739cx15] == _0xb455[12]) {
+      result = await p(_0x739cx15, _0x739cx10);
+    } else {
+      if (m[_0x739cx15] == _0xb455[14]) {
+        result = await q(_0x739cx15);
+      }
+    }
+    let { status, vipcode, message } = result;
+    if (status) {
+      clearInterval(interval);
+      d[_0xb455[24]] = vipcode;
+      b[_0xb455[27]] = _0xb455[35];
+      v();
+    } else {
+      i[_0xb455[24]] = message;
+      f[_0xb455[34]][_0xb455[33]](_0xb455[32]);
+    }
+  } else {
+    i[_0xb455[24]] = _0xb455[36];
+    f[_0xb455[34]][_0xb455[33]](_0xb455[32]);
+  }
+});
+e[_0xb455[37]](_0xb455[23], () => {
+  const _0x739cx17 = d[_0xb455[24]][_0xb455[28]]();
+  if (_0x739cx17[_0xb455[30]] > 0) {
+    navigator[_0xb455[39]][_0xb455[38]](_0x739cx17);
+    e[_0xb455[24]] = _0xb455[40];
+  }
+});
+g[_0xb455[37]](_0xb455[23], () => {
+  f[_0xb455[34]][_0xb455[41]](_0xb455[32]);
+});
+h[_0xb455[37]](_0xb455[23], () => {
+  f[_0xb455[34]][_0xb455[41]](_0xb455[32]);
+});
+a[_0xb455[37]](_0xb455[42], () => {
+  const _0x739cx14 = a[_0xb455[27]];
+  if (_0x739cx14 == _0xb455[14]) {
+    k[_0xb455[34]][_0xb455[41]](_0xb455[32]);
+    l[_0xb455[34]][_0xb455[33]](_0xb455[32]);
+  } else {
+    k[_0xb455[34]][_0xb455[33]](_0xb455[32]);
+    l[_0xb455[34]][_0xb455[41]](_0xb455[32]);
+  }
+});
+const s = document[_0xb455[1]](_0xb455[43]);
+const t = document[_0xb455[1]](_0xb455[44]);
+const r = document[_0xb455[1]](_0xb455[45]);
+const v = () => {
+  let _0x739cx1c = 59;
+  s[_0xb455[47]][_0xb455[46]] = _0xb455[48];
+  r[_0xb455[24]] = `${_0xb455[49]}`;
+  const _0x739cx1d = () => {
+    if (_0x739cx1c > 0) {
+      s[_0xb455[24]] = `${_0xb455[35]}${_0x739cx1c}${_0xb455[50]}`;
+      _0x739cx1c--;
+    } else {
+      t[_0xb455[24]] = `${_0xb455[51]}`;
+      r[_0xb455[24]] = `${_0xb455[35]}`;
+      s[_0xb455[47]][_0xb455[46]] = _0xb455[52];
+      s[_0xb455[24]] = `${_0xb455[35]}`;
+      clearInterval(interval);
+    }
+  };
+  interval = setInterval(_0x739cx1d, 1000);
+};
